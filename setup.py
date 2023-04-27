c0330e0efc81c6b2dfe7cd883591f355afad1f1f
Nash242 <nimish242tambe@gmail.com>
2023-04-27 11:08:52 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Wine package",
+    author="Nash242",
+    packages=find_packages(),
+)
